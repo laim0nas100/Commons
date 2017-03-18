5cfd9895e9a41240cb56c9122f2211d4e8659414
laim0nas100 <aciukadsiunciate@gmail.com>
2017-03-18 17:04:43 +0200

    precise time

--- src/LibraryLB/Log.java
+++ src/LibraryLB/Log.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -67,30 +68,39 @@ public class Log extends PrintStream{
     }
     
     public static void write(Object...objects){
+        long millis = System.currentTimeMillis();
         String string = "";
         for(Object s:objects){  
             string+=" ,"+s;
         }
-        Log.writeln(string.substring(2));
+        logThis(string.substring(2), millis);
     }
     public static void writeln(Object...objects){
-            for(Object s:objects){
-                Runnable r = () ->{
-                    String time =  getZonedDateTime("HH:mm:ss:S");
-                    String string = time+"["+String.valueOf(s)+"]";
-                    if(display){
-                        System.out.println(string);
-                    }
-                    Log.INSTANCE.list.add(string);
-                    if(!console){
-                        flushBuffer();
-                    }
-                };
-                exe.submit(r);
+        long millis = System.currentTimeMillis();
+        for(Object s:objects){
+            logThis(String.valueOf(s), millis);
+        } 
+    }
+    private static void logThis(String string, long millis){
+        
+        Runnable r = () ->{         
+            String time = getZonedDateTime("HHmmss-SSS",millis);
+            String res = time+"{"+String.valueOf(string)+"}";
+            if(display){
+                System.out.println(res);
             }
+            Log.INSTANCE.list.add(res);
+            if(!console){
+                flushBuffer();
+            }
+        };
+        exe.submit(r);
     }
     public static String getZonedDateTime(String format){
         return ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern(format));
+    }
+    public static String getZonedDateTime(String format, long millis){
+        return Instant.ofEpochMilli(millis).atZone(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern(format));
     }
     public static void printProperties(Properties properties){
         Object[] toArray = properties.keySet().toArray();
