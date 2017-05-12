3086b47ab59f6f22e5400171f5a689e98f471ece
laim0nas100 <aciukadsiunciate@gmail.com>
2017-05-13 01:34:37 +0300

    SceneManagement

---
+++ src/LibraryLB/FX/SceneManagement/PosProperty.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LibraryLB.FX.SceneManagement;
+
+import javafx.beans.property.SimpleDoubleProperty;
+
+/**
+ *
+ * @author Lemmin
+ */
+public class PosProperty {
+    public SimpleDoubleProperty x,y;
+    public PosProperty(double X, double Y){
+        this.x = new SimpleDoubleProperty(X);
+        this.y = new SimpleDoubleProperty(Y);
+    }
+
+    @Override
+    public String toString(){
+        return "["+x.get()+":"+y.get()+"]";
+    }
+}
