190e3296ce01c5d177e4a52c49e512f3d846c9fa
laim0nas100 <aciukadsiunciate@gmail.com>
2018-08-22 17:20:50 +0300

    indentation adjust

--- commons-core/src/test/java/ArrayTest.java
+++ commons-core/src/test/java/ArrayTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Laimonas-Beniusis-PC
@@ -19,24 +18,24 @@ import org.junit.Test;
 public class ArrayTest {
 
     @Test
-    public void arrayTest() throws InterruptedException{
-        Integer[] arr = new Integer[]{1,2,3};
+    public void arrayTest() throws InterruptedException {
+        Integer[] arr = new Integer[]{1, 2, 3};
         Log.print(arr);
         List list = new ArrayList<>();
         list.add(5);
         list.add(6);
         list.add(7);
         Object[] newArray = ArrayOp.newArray(list);
-        Log.print("list",list);
+        Log.print("list", list);
         Log.print("Array");
         Log.print(newArray);
         Log.print("Merged");
         Integer[] merge = ArrayOp.merge(arr, ArrayOp.castArray(newArray, Integer.class));
         Log.print(merge);
         Log.print("Remove 2");
-        Log.print(ArrayOp.removeByIndex(merge, 2,2));
-         Log.print(merge);
-         
-         Log.await(1, TimeUnit.HOURS);
+        Log.print(ArrayOp.removeByIndex(merge, 2, 2));
+        Log.print(merge);
+
+        Log.await(1, TimeUnit.HOURS);
     }
 }
