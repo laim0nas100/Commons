cbf04eb77dac5b0fc2c105ffd777bc98c76b1394
laim0nas100 <aciukadsiunciate@gmail.com>
2018-09-21 11:43:02 +0300

    Equator interface

---
+++ commonslb-core/src/main/java/lt/lb/commons/interfaces/Equator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.lb.commons.interfaces;
+
+/**
+ *
+ * @author Laimonas-Beniusis-PC
+ * @param <T> type
+ */
+public interface Equator <T>{
+    public boolean equals(T value1, T value2);
+}
