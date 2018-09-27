f46f5bac15ab795b636e8ba8b6e70d33f9cf8282
laim0nas100 <aciukadsiunciate@gmail.com>
2018-09-27 17:10:38 +0300

    add test

---
+++ commonslb-graphtheory/src/test/java/graphtest/SimpleGraphGenerate.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphtest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import lt.lb.commons.Log;
+import lt.lb.commons.graphtheory.GLink;
+import lt.lb.commons.graphtheory.Orgraph;
+import lt.lb.commons.graphtheory.paths.GraphGenerator;
+import lt.lb.commons.misc.F;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Laimonas-Beniusis-PC
+ */
+public class SimpleGraphGenerate {
+    
+    public SimpleGraphGenerate() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    
+    static{
+        Log.instant = true;
+    }
+    @Test
+    public void generateSimple(){
+        Orgraph gr = new Orgraph();
+        GraphGenerator.generateSimpleConnected(F.RND.RND, gr, 50, ()->1d);
+        
+        
+        
+        ArrayList<GLink> links = new ArrayList<>(gr.links.values());
+        Collections.sort(links,(a,b)->{
+            int c = (int) (a.nodeFrom - b.nodeFrom);
+            if(c == 0){
+                c = (int) (a.nodeTo - b.nodeTo);
+            }
+            
+            return c;
+        });
+        Log.printLines(links);
+        
+        
+        
+    }
+}
