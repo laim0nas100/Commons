3bd3bdedb2bf41ff1a155812df60674a68ebfe09
laim0nas100 <aciukadsiunciate@gmail.com>
2018-10-11 21:41:51 +0300

    update

---
+++ commonslb-tests/src/test/java/core/NumberValueTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import java.util.concurrent.TimeUnit;
+import lt.lb.commons.F;
+import lt.lb.commons.Log;
+import lt.lb.commons.containers.NumberValue;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lemmin
+ */
+public class NumberValueTest {
+    
+    public NumberValueTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testMe(){
+        NumberValue<Integer> numberVal = NumberValue.of(10);
+        Log.print(numberVal.incrementAndGet());
+        Log.print(numberVal.incrementAndGet());
+        Log.print(numberVal.incrementAndGet());
+        Log.print(numberVal.incrementAndGet());
+        
+        byte b = 0xa;
+        Log.print(numberVal.incrementAndGet(10));
+        
+        F.unsafeRun(()->{
+            Log.await(1, TimeUnit.HOURS);
+        });
+        
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
