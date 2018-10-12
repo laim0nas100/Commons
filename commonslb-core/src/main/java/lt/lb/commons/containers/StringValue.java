5369fa4788c80121d2ff9e0999e953c8a92202b1
laim0nas100 <aciukadsiunciate@gmail.com>
2018-10-12 09:00:42 +0300

    Add string value

---
+++ commonslb-core/src/main/java/lt/lb/commons/containers/StringValue.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.lb.commons.containers;
+
+/**
+ *
+ * @author laim0nas100
+ */
+public class StringValue extends Value<String> {
+
+    public StringValue() {
+        super();
+    }
+
+    public StringValue(String val) {
+        super(val);
+    }
+
+    public String appendAndGet(String str) {
+        return this.setAndGet(() -> this.get() + str);
+    }
+    
+    public String getAndAppend(String str){
+        return this.getAndSet(() -> this.get() + str);
+    }
+
+}
