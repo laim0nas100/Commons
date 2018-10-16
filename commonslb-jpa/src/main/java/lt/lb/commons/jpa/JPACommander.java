6d732ad799c85bf15d90bcc5a0efbb3114ba2032
laim0nas100 <aciukadsiunciate@gmail.com>
2018-10-16 22:21:42 +0300

    add JPACommander constructor

--- commonslb-jpa/src/main/java/lt/lb/commons/jpa/JPACommander.java
+++ commonslb-jpa/src/main/java/lt/lb/commons/jpa/JPACommander.java
@@ -17,6 +17,45 @@ import lt.lb.commons.jpa.decorators.IQueryDecorator;
  */
 public interface JPACommander<T,PrimeKeyT> {
     
+    public static <T,PrimeKey> JPACommander<T,PrimeKey> of(final Class<T> cls,JPACommands commands){
+        return new JPACommander<T, PrimeKey>() {
+            @Override
+            public List<T> search(int start, int pageSize, IQueryDecorator<T>... predicates) {
+                return commands.search(cls, start, pageSize, predicates);
+            }
+
+            @Override
+            public List<T> search(IQueryDecorator<T>... predicates) {
+                return commands.search(cls, predicates);
+            }
+
+            @Override
+            public void persist(T obj) {
+                commands.persist(obj);
+            }
+
+            @Override
+            public T find(PrimeKey primaryKey) {
+                return commands.find(cls, primaryKey);
+            }
+
+            @Override
+            public Long count(IQueryDecorator<T>... predicates) {
+                return commands.count(cls, predicates);
+            }
+
+            @Override
+            public void remove(PrimeKey primaryKey) {
+                commands.remove(cls, primaryKey);
+            }
+
+            @Override
+            public void merge(T obj) {
+                commands.merge(obj);
+            }
+        };
+    }
+    
     public List<T> search(int start, int pageSize, IQueryDecorator<T>... predicates);
 
     public List<T> search(IQueryDecorator<T>... predicates);
