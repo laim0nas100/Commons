48aadbad0e0db666656e7f6cc3bba00f40cbb2dc
laim0nas100 <aciukadsiunciate@gmail.com>
2018-10-22 11:17:01 +0300

    Add BooleanValue, add more NumberValue methods

---
+++ commonslb-core/src/main/java/lt/lb/commons/containers/BooleanValue.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.lb.commons.containers;
+
+/**
+ *
+ * @author laim0nas100
+ */
+public class BooleanValue extends Value<Boolean> {
+    
+    public static BooleanValue TRUE(){
+        return new BooleanValue(Boolean.TRUE);
+    }
+    
+    public static BooleanValue FALSE(){
+        return new BooleanValue(Boolean.FALSE);
+    }
+
+    public BooleanValue() {
+        this(false);
+    }
+
+    public BooleanValue(Boolean bool) {
+        if (bool == null) {
+            this.value = Boolean.FALSE;
+        } else {
+            this.value = bool;
+        }
+    }
+
+    public Boolean not() {
+        return !this.get();
+    }
+
+    public void setFalse() {
+        this.set(Boolean.FALSE);
+    }
+
+    public void setTrue() {
+        this.set(Boolean.TRUE);
+    }
+
+    public Boolean negateAndGet() {
+        return this.setAndGet(() -> this.not());
+    }
+
+    public Boolean getAndNegate() {
+        return this.getAndSet(() -> this.not());
+    }
+
+}
--- commonslb-core/src/main/java/lt/lb/commons/containers/NumberValue.java
+++ commonslb-core/src/main/java/lt/lb/commons/containers/NumberValue.java
@@ -13,23 +13,37 @@ import lt.lb.commons.NumberOp;
  * @param <T>
  */
 public class NumberValue<T extends Number> extends Value<T> {
-    
-    
-    public static <F extends Number> NumberValue<F> of(F i){
+
+    public static <F extends Number> NumberValue<F> of(F i) {
         return new NumberValue<>(i);
     }
-    
-    
-    public NumberValue(){
+
+    public NumberValue() {
         super();
     }
-    
+
     public NumberValue(T val) {
         super(val);
     }
 
     public T incrementAndGet() {
         return incrementAndGet(1);
+    }
+
+    public T getAndIncrement() {
+        return getAndIncrement(1);
+    }
+
+    public T getAndIncrement(Number n) {
+        return getAndSet(() -> NumberOp.add(get(), n));
+    }
+
+    public T getAndDecrement() {
+        return getAndDecrement(1);
+    }
+
+    public T getAndDecrement(Number n) {
+        return getAndSet(() -> NumberOp.subtract(get(), n));
     }
 
     public T incrementAndGet(Number n) {
@@ -43,15 +57,29 @@ public class NumberValue<T extends Number> extends Value<T> {
     public T decrementAndGet() {
         return decrementAndGet(1);
     }
-    
-    public T multiplyAndGet(Number n){
+
+    public T multiplyAndGet(Number n) {
         return setAndGet(() -> NumberOp.multiply(get(), n));
     }
-    
-    public T divideAndGet(Number n){
+
+    public T getAndMultiply(Number n) {
+        return getAndSet(() -> NumberOp.multiply(get(), n));
+    }
+
+    public T divideAndGet(Number n) {
         return setAndGet(() -> NumberOp.divide(get(), n));
     }
 
-    
+    public T getAndDivide(Number n) {
+        return getAndSet(() -> NumberOp.divide(get(), n));
+    }
+
+    public T modAndGet(Number n) {
+        return setAndGet(() -> NumberOp.modulus(get(), n));
+    }
+
+    public T getAndMod(Number n) {
+        return getAndSet(() -> NumberOp.modulus(get(), n));
+    }
 
 }
