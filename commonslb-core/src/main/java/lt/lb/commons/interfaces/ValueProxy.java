7c248cb4b0f8e693964b324f325d5a627f7c2dd6
laim0nas100 <aciukadsiunciate@gmail.com>
2019-04-17 12:01:30 +0300

    add Optional helper method

--- commonslb-core/src/main/java/lt/lb/commons/interfaces/ValueProxy.java
+++ commonslb-core/src/main/java/lt/lb/commons/interfaces/ValueProxy.java
@@ -1,5 +1,6 @@
 package lt.lb.commons.interfaces;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -8,30 +9,36 @@ import java.util.function.Supplier;
  * @author laim0nas100
  */
 public interface ValueProxy<T> extends Supplier<T>, Consumer<T> {
-    
+
     /**
      * Main get method
-     * @return 
+     *
+     * @return
      */
     @Override
     public T get();
-    
+
     /**
      * Main set method
-     * @param v 
+     *
+     * @param v
      */
     public void set(T v);
-    
+
     public default T getValue() {
         return this.get();
     }
-    
+
     public default void setValue(T v) {
         this.set(v);
     }
-    
+
     @Override
     public default void accept(T v) {
         this.set(v);
+    }
+
+    public default Optional<T> toOptional() {
+        return Optional.ofNullable(get());
     }
 }
