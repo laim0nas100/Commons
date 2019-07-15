c8a53d7fe0416898f05dcd20c7652a38551ef694
laim0nas100 <aciukadsiunciate@gmail.com>
2019-07-15 16:37:46 +0300

    visiting all nodes tree visitor factory method

--- commonslb-core/src/main/java/lt/lb/commons/iteration/TreeVisitor.java
+++ commonslb-core/src/main/java/lt/lb/commons/iteration/TreeVisitor.java
@@ -1,7 +1,10 @@
 package lt.lb.commons.iteration;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import lt.lb.commons.iteration.impl.TreeVisitorImpl;
 
 /**
@@ -12,15 +15,17 @@ public interface TreeVisitor<T> extends Visitor<T> {
 
     /**
      * How to traverse deeper.
+     *
      * @param item
-     * @return 
+     * @return
      */
     public ReadOnlyIterator<T> getChildrenIterator(T item);
 
     /**
      * Depth-first search iterative. Same as preorder traversal.
+     *
      * @param root
-     * @return 
+     * @return
      */
     public default Optional<T> DFSIterative(T root) {
         return TreeVisitorImpl.DFSIterative(this, root);
@@ -28,8 +33,9 @@ public interface TreeVisitor<T> extends Visitor<T> {
 
     /**
      * Depth-first search recursive. Same as preorder traversal.
+     *
      * @param root
-     * @return 
+     * @return
      */
     public default Optional<T> DFS(T root) {
         return TreeVisitorImpl.DFS(this, root);
@@ -37,31 +43,33 @@ public interface TreeVisitor<T> extends Visitor<T> {
 
     /**
      * Breath-first search.
+     *
      * @param root
-     * @return 
+     * @return
      */
     public default Optional<T> BFS(T root) {
         return TreeVisitorImpl.BFS(this, root);
     }
-    
+
     /**
      * PosOrder search (Children first). Recursive.
+     *
      * @param root
-     * @return 
+     * @return
      */
-    public default Optional<T> PosOrder(T root){
+    public default Optional<T> PosOrder(T root) {
         return TreeVisitorImpl.PostOrder(this, root);
     }
-    
+
     /**
      * PosOrder search (Children first). Iterative.
+     *
      * @param root
-     * @return 
+     * @return
      */
-    public default Optional<T> PosOrderIterative(T root){
+    public default Optional<T> PosOrderIterative(T root) {
         return TreeVisitorImpl.PostOrderIterative(this, root);
     }
-
 
     public static <T> TreeVisitor<T> of(Visitor<T> visit, Function<? super T, ReadOnlyIterator<T>> childrenGetter) {
         return new TreeVisitor<T>() {
@@ -77,6 +85,18 @@ public interface TreeVisitor<T> extends Visitor<T> {
         };
     }
 
+    public static <T> TreeVisitor<T> ofAll(Consumer<T> cons, Function<? super T, ReadOnlyIterator<T>> childrenGetter) {
+        return new TreeVisitor<T>() {
+            @Override
+            public Boolean find(T item) {
+                cons.accept(item);
+                return false;
+            }
+
+            @Override
+            public ReadOnlyIterator<T> getChildrenIterator(T item) {
+                return childrenGetter.apply(item);
+            }
+        };
+    }
 }
-
-
