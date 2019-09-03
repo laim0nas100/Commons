ffd971deeb0514dfe0f44aea4c133f171bc4a422
laim0nas100 <aciukadsiunciate@gmail.com>
2019-09-03 10:35:25 +0300

    use synchronized

--- commonslb-core/src/main/java/lt/lb/commons/containers/caching/LazyValue.java
+++ commonslb-core/src/main/java/lt/lb/commons/containers/caching/LazyValue.java
@@ -1,14 +1,8 @@
 package lt.lb.commons.containers.caching;
 
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
-import lt.lb.commons.F;
 import lt.lb.commons.Java;
-import lt.lb.commons.Timer;
 import lt.lb.commons.containers.values.Value;
-import lt.lb.commons.threads.Futures;
 
 /**
  * Value that loads after being called. Can manually set it.
@@ -21,9 +15,6 @@ public class LazyValue<T> extends Value<T> {
     protected Long loaded = null;
     protected Supplier<Boolean> loader = () -> loaded != null && loaded <= Java.getNanoTime();
     protected Supplier<T> supply;
-
-    protected AtomicBoolean inGet = new AtomicBoolean(false);
-    protected volatile Future<T> future;
 
     public LazyValue(Supplier<T> supply) {
         this.supply = supply;
@@ -50,23 +41,9 @@ public class LazyValue<T> extends Value<T> {
      * @return
      */
     @Override
-    public T get() {
-        if (!loader.get() || future == null) {
-            if (inGet.compareAndSet(false, true)) {
-                FutureTask<T> fut = Futures.ofSupplier(() -> {
-                    return super.setAndGet(supply);
-                });
-                fut.run();
-                future = fut;
-
-                inGet.set(false);
-                F.checkedRun(() -> {
-                    future.get();
-                });
-
-            } else {
-                return F.unsafeCall(() -> future.get());
-            }
+    public synchronized T get() {
+        if (!loader.get()) {
+            return super.setAndGet(supply);
         }
 
         return super.get();
