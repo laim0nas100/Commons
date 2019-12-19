dc0002f2d944d37c06635f147fffeb3ca5d335dc
laim0nas100 <aciukadsiunciate@gmail.com>
2019-12-19 13:57:34 +0200

    Add StringProducer implementation, for lazy evaluation and formatting strings

---
+++ commonslb-tests/src/test/java/regression/lex/StringProcuderTest.java
@@ -0,0 +1,36 @@
+package regression.lex;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import lt.lb.commons.parsing.explicit.StringProducer;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+
+/**
+ *
+ * @author laim0nas100
+ */
+public class StringProcuderTest {
+    
+    public static class SimpleCtx {
+        
+        String name;
+        Date now = new Date();
+    }
+    
+    @Test
+    public void test() throws Exception {
+        SimpleCtx context = new SimpleCtx();
+        context.name = "Sailor";
+        
+        StringProducer<SimpleCtx> hello = StringProducer.ofConstant("Hello");
+        StringProducer<SimpleCtx> space = StringProducer.ofConstant(" ");
+        StringProducer<SimpleCtx> name = ctx -> ctx.name;
+        StringProducer<SimpleCtx> date = ctx -> new SimpleDateFormat("YY").format(ctx.now);
+        
+        StringProducer<SimpleCtx> ofAll = StringProducer.ofAll(hello, space, name, StringProducer.ofConstant(" in year "), date);
+        
+        assertThat(ofAll.apply(context)).isEqualTo("Hello Sailor in year " + new SimpleDateFormat("YY").format(context.now));
+        
+    }
+}
