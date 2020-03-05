00f996b23f8c811c4ebd120a2be2c096b8eedcf6
laim0nas100 <aciukadsiunciate@gmail.com>
2020-03-05 15:50:41 +0200

    CloneSupport new with less restrictions

--- commonslb-core/src/main/java/lt/lb/commons/interfaces/CloneSupport.java
+++ commonslb-core/src/main/java/lt/lb/commons/interfaces/CloneSupport.java
@@ -12,7 +12,9 @@ import lt.lb.commons.iteration.ReadOnlyIterator;
  * Interface for cloning, explicitly declaring clone method publicly.
  *
  * @author laim0nas100
+ * @param <T>
  */
+@FunctionalInterface
 public interface CloneSupport<T> extends Cloneable {
 
     public T clone();
@@ -21,17 +23,17 @@ public interface CloneSupport<T> extends Cloneable {
      * Clone or return a null
      *
      * @param <A> item type
+     * @param <D> type that produces a cloned item
      * @param obj object to be cloned
      * @return cloned object or null
      */
-    public static <A extends CloneSupport<A>> A cloneOrNull(A obj) {
+    public static <A, D extends CloneSupport<A>> A cloneOrNull(D obj) {
         return obj == null ? null : obj.clone();
     }
 
     /**
-     * Analogous to cloneOrNull. Also performs a cast.
-     * Implies that the cloning function returns correct type. No explicit
-     * compile-time type checking.
+     * Analogous to cloneOrNull. Also performs a cast. Implies that the cloning
+     * function returns correct type. No explicit compile-time type checking.
      *
      * @param <A> item type
      * @param obj object to be cloned
@@ -46,12 +48,13 @@ public interface CloneSupport<T> extends Cloneable {
      * Clone using provided function or juts return a null. Useful when objects
      * have non-standard way of cloning themselves.
      *
-     * @param <A> item type
+     * @param <D> parameter item type
+     * @param <A> result item type
      * @param obj object to be cloned
      * @param cloningFunction cloning function
      * @return cloned object or null
      */
-    public static <A> A cloneOrNull(A obj, Function<? super A, A> cloningFunction) {
+    public static <D, A> A cloneOrNull(D obj, Function<? super D, A> cloningFunction) {
         return obj == null ? null : cloningFunction.apply(obj);
     }
 
@@ -59,28 +62,32 @@ public interface CloneSupport<T> extends Cloneable {
      * Clones an iterable to a collection.
      *
      * @param <A> item type
+     * @param <D> item that produces a clone type
      * @param <C> collection type
      * @param iter iterable with items
      * @param collectionSupplier new collection supplier
      * @return
      */
-    public static <A extends CloneSupport<A>, C extends Collection<A>> C cloneCollection(Iterable<A> iter, Supplier<? extends C> collectionSupplier) {
+    public static <A, D extends CloneSupport<A>, C extends Collection<A>> C cloneCollection(Iterable<D> iter, Supplier<? extends C> collectionSupplier) {
         return cloneAll(iter, collectionSupplier, (item, sink) -> sink.add(item), CloneSupport::clone);
     }
 
     /**
      * Clones a map with very specific cloning details.
      *
-     * @param <K> key type
-     * @param <A> value type
-     * @param <C> collection type
+     * @param <K> parameter key type
+     * @param <KK> result key type
+     * @param <A> parameter value type
+     * @param <AA> parameter value type
+     * @param <C> result map type
+     * @param <CC> parameter map type
      * @param map map with items
      * @param collectionSupplier new map supplier
      * @param keyCloningFuncion key cloning function
      * @param cloningFuncion value cloning function
      * @return
      */
-    public static <K, A, C extends Map<K, A>> C cloneMap(Map<K, A> map, Supplier<? extends C> collectionSupplier, Function<K, K> keyCloningFuncion, Function<A, A> cloningFuncion) {
+    public static <K, KK, A, AA, C extends Map<K, A>, CC extends Map<KK, AA>> C cloneMap(CC map, Supplier<? extends C> collectionSupplier, Function<KK, K> keyCloningFuncion, Function<AA, A> cloningFuncion) {
 
         C cloned = collectionSupplier.get();
 
@@ -112,13 +119,14 @@ public interface CloneSupport<T> extends Cloneable {
      * Clones a map with immutable keys and CloneSupport type values.
      *
      * @param <K> key type
-     * @param <A> value type
+     * @param <A> result value type
+     * @param <AA> value type that produces a clone
      * @param <C> collection type
      * @param map map with items
      * @param collectionSupplier new map supplier
      * @return
      */
-    public static <K, A extends CloneSupport<A>, C extends Map<K, A>> C cloneMapImmutableSupported(Map<K, A> map, Supplier<? extends C> collectionSupplier) {
+    public static <K, A, AA extends CloneSupport<A>, C extends Map<K, A>> C cloneMapImmutableSupported(Map<K, AA> map, Supplier<? extends C> collectionSupplier) {
         return cloneMap(map, collectionSupplier, k -> k, CloneSupport::clone);
     }
 
@@ -126,25 +134,28 @@ public interface CloneSupport<T> extends Cloneable {
      * Clones a map with immutable keys and CloneSupport type values and keys.
      *
      * @param <K> key type
-     * @param <A> value type
+     * @param <KK> key type that produces a clone
+     * @param <A> result value type
+     * @param <AA> value type that produces a clone
      * @param <C> collection type
      * @param map map with items
      * @param collectionSupplier new map supplier
      * @return
      */
-    public static <K extends CloneSupport<K>, A extends CloneSupport<A>, C extends Map<K, A>> C cloneMapSupported(Map<K, A> map, Supplier<? extends C> collectionSupplier) {
+    public static <K, KK extends CloneSupport<K>, A, AA extends CloneSupport<A>, C extends Map<K, A>> C cloneMapSupported(Map<KK, AA> map, Supplier<? extends C> collectionSupplier) {
         return cloneMap(map, collectionSupplier, CloneSupport::clone, CloneSupport::clone);
     }
 
     /**
      * Clones an array.
      *
-     * @param <A> item type
+     * @param <A> item that supports clone type
+     * @param <D> expected clone result type
      * @param iter array with items
      * @param arraySupplier array maker
      * @return
      */
-    public static <A extends CloneSupport<A>> A[] cloneArray(A[] iter, Supplier<A[]> arraySupplier) {
+    public static <A, D extends CloneSupport<A>> A[] cloneArray(D[] iter, Supplier<A[]> arraySupplier) {
         IntegerValue i = new IntegerValue(0);
         return cloneAll(ReadOnlyIterator.of(iter), arraySupplier, (item, sink) -> sink[i.getAndIncrement()] = item, CloneSupport::clone);
 
@@ -153,7 +164,8 @@ public interface CloneSupport<T> extends Cloneable {
     /**
      * Clones an iterable to specified sink with specified methodology.
      *
-     * @param <A> item type
+     * @param <A> item that supports clone type
+     * @param <D> expected clone result type
      * @param <C> new collection
      * @param iter iterable with items
      * @param sinkSupplier new collection supplier
@@ -161,7 +173,7 @@ public interface CloneSupport<T> extends Cloneable {
      * @param cloningFuncion how to clone item
      * @return
      */
-    public static <A extends CloneSupport<A>, C> C cloneAll(Iterable<A> iter, Supplier<C> sinkSupplier, BiConsumer<A, C> consumer, Function<A, A> cloningFuncion) {
+    public static <A, D extends CloneSupport<A>, C> C cloneAll(Iterable<D> iter, Supplier<C> sinkSupplier, BiConsumer<A, C> consumer, Function<D, A> cloningFuncion) {
         C get = sinkSupplier.get();
         iter.forEach(item -> {
             consumer.accept(CloneSupport.cloneOrNull(item, cloningFuncion), get);
