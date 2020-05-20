8d7474872a18d4cc8b6e1f2b230de9e75b6ca096
laim0nas100 <aciukadsiunciate@gmail.com>
2020-05-21 01:21:09 +0300

    Add simple test

--- commonslb-tests/src/test/java/regression/core/StreamMapperTest.java
+++ commonslb-tests/src/test/java/regression/core/StreamMapperTest.java
@@ -2,12 +2,15 @@ package regression.core;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lt.lb.commons.func.StreamMapper;
 import lt.lb.commons.func.StreamMapperEnder;
+import lt.lb.commons.func.StreamMappers;
 import static lt.lb.commons.func.StreamMappers.concat;
 import static lt.lb.commons.func.StreamMappers.distinct;
+import lt.lb.commons.interfaces.Equator;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
@@ -33,7 +36,6 @@ public class StreamMapperTest {
                 .filter(m -> m <= 45.1)
                 .sorted();
 
-
         StreamMapperEnder<Integer, Double, List<Double>> collect = filter.thenCombine(sorted).collect(Collectors.toList());
 
         assertThat(collect.startingWithOpt(list)).isEqualTo(Arrays.asList(25.0, 30.0, 35.0, 40.0, 45.0));
@@ -47,7 +49,7 @@ public class StreamMapperTest {
 
         StreamMapper<Integer, String> filter = StreamMapper.of(Integer.class)
                 .map(m -> m * 10)
-                .map(m->m)
+                .map(m -> m)
                 .apply(distinct((a, b) -> a % 5 == b % 5)) // dsitinct by equality modulo 5
                 .apply(concat(7))
                 .map(m -> "" + m);
@@ -64,13 +66,12 @@ public class StreamMapperTest {
         assertThat(decorate.collect(Collectors.toList())).isEqualTo(Arrays.asList(2.0, 5.0, 7.0, 10.0));
 
     }
-    
+
     @Test
-    public void test3(){
+    public void test3() {
         StreamMapper<Integer, String> filter = StreamMapper.of(Integer.class)
-                .apply(concat(1,2,3,4,5))
+                .apply(concat(1, 2, 3, 4, 5))
                 .map(m -> m * 10)
-                
                 .apply(distinct((a, b) -> a % 5 == b % 5)) // dsitinct by equality modulo 5
                 .apply(concat(7))
                 .map(m -> "" + m);
@@ -85,5 +86,31 @@ public class StreamMapperTest {
         List<Double> startingWith = combine.collect(Collectors.toList()).startingWithOpt(l);
 
         assertThat(startingWith).isEqualTo(Arrays.asList(2.0, 5.0, 7.0, 10.0));
+    }
+
+    private static class TestClass4 {
+
+        public int i;
+
+        public TestClass4(int num) {
+            i = num;
+        }
+    }
+
+    @Test
+    public void test4() {
+        StreamMapper<Integer, TestClass4> map1 = StreamMapper.of(Integer.class)
+                .apply(concat(1, 2, 3, 4, 5))
+                .map(TestClass4::new);
+
+        Set<TestClass4> startingWith = StreamMapper.of(Integer.class)
+                .apply(concat(1, 2, 3, 4, 5))
+                .map(m -> m * 2)
+                .map(TestClass4::new).collectToSet().startingWith();
+
+        List<Integer> left = map1.apply(StreamMappers.filterNotIn(startingWith, Equator.valueHashEquator(a -> a.i)))
+                .map(m -> m.i).collectToList().startingWith();
+
+        assertThat(left).isEqualTo(Arrays.asList(1, 3, 5));
     }
 }
