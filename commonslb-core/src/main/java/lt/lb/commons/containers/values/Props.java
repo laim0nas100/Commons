e0986a5a31359c2a88bdeee186d50bd3b60ef001
laim0nas100 <aciukadsiunciate@gmail.com>
2020-07-09 12:59:47 +0300

    update methods and documentation

--- commonslb-core/src/main/java/lt/lb/commons/containers/values/Props.java
+++ commonslb-core/src/main/java/lt/lb/commons/containers/values/Props.java
@@ -11,13 +11,19 @@ import java.util.Objects;
 public class Props extends HashMap<String, Value> {
 
     /**
-     * Explicit typing and property key information.
-     * Designed to be used with Props.
+     * Explicit typing and property key information. Designed to be used with
+     * Props.
      *
      * @param <T>
      */
     public static class PropGet<T> {
 
+        /**
+         * Construct PropGet of specified key and explicit type.
+         * @param <Type>
+         * @param str
+         * @return 
+         */
         public static <Type> PropGet<Type> of(String str) {
             return new PropGet<>(str);
         }
@@ -27,27 +33,91 @@ public class Props extends HashMap<String, Value> {
             this.propKey = Objects.requireNonNull(propKey);
         }
 
+       /**
+         * Inserts new value at given Props object by this key and returns newly inserted value proxy.
+         * @param props
+         * @param value
+         * @return 
+         */
         public Value<T> insert(Props props, T value) {
-            Value<T> insert = props.insert(propKey, value);
-            return insert;
+            return props.insert(propKey, value);
         }
 
+        /**
+         * Removes value proxy from Props object by this key and returns it.
+         * @param props
+         * @return 
+         */
+        public Value<T> remove(Props props) {
+            return props.remove(propKey);
+        }
+
+        /**
+         * Removes value from Props object by this key and returns it.
+         * @param props
+         * @return 
+         */
+        public T removeGet(Props props) {
+            Value<T> remove = props.remove(propKey);
+            if (remove == null) {
+                return null;
+            }
+            return remove.get();
+        }
+
+        /**
+         * Inserts new value at given Props object by this key and returns newly inserted value.
+         * @param props
+         * @param value
+         * @return 
+         */
+        public T insertGet(Props props, T value) {
+            return (T) props.insert(propKey, value).get();
+        }
+
+        /**
+         * Gets and casts value from given Props object by this key.
+         * @param props
+         * @return 
+         */
         public T get(Props props) {
             return props.getCast(propKey);
         }
 
+        /**
+         * Gets proxy value from given Props object by his key.
+         * @param props
+         * @return 
+         */
         public Value<T> getAsValue(Props props) {
             return props.get(propKey);
         }
 
     }
 
-    public Value insert(String key, Object value) {
-        Value val = new Value(value);
+    /**
+     * Insert value and return proxy object to that new value
+     *
+     * @param <T>
+     * @param key
+     * @param value
+     * @return
+     */
+    public <T> Value<T> insert(String key, T value) {
+        Value<T> val = new Value<>(value);
         this.put(key, val);
         return val;
     }
 
+    /**
+     * Retrieves value, unless its key is not present, then it returns default
+     * value.
+     *
+     * @param <T>
+     * @param str
+     * @param val
+     * @return
+     */
     public <T> T getOrDefaultCast(String str, T val) {
         Value get = this.get(str);
         if (get == null) {
@@ -56,6 +126,13 @@ public class Props extends HashMap<String, Value> {
         return (T) get.get();
     }
 
+    /**
+     * Retrieves and casts value by given key.
+     *
+     * @param <T>
+     * @param str
+     * @return
+     */
     public <T> T getCast(String str) {
         Value get = this.get(str);
         if (get == null) {
