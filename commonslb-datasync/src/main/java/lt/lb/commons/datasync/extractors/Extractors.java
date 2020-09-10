37db2daf507d4a1bb43b284949bb3b8e7a4c2d07
laim0nas100 <aciukadsiunciate@gmail.com>
2020-09-10 15:02:10 +0300

    implement manual property access because PropertyDescriptor does not work

--- commonslb-datasync/src/main/java/lt/lb/commons/datasync/extractors/Extractors.java
+++ commonslb-datasync/src/main/java/lt/lb/commons/datasync/extractors/Extractors.java
@@ -1,9 +1,17 @@
 package lt.lb.commons.datasync.extractors;
 
+import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import static java.util.Locale.ENGLISH;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 import lt.lb.commons.F;
+import lt.lb.commons.containers.caching.LazyValue;
 import lt.lb.commons.containers.values.ValueProxy;
 import lt.lb.commons.reflect.FieldChain;
 
@@ -12,8 +20,8 @@ import lt.lb.commons.reflect.FieldChain;
  * @author laim0nas100
  */
 public abstract class Extractors {
-    
-    public static <T> ValueProxy<Collection<T>> mutableCollection(Supplier<? extends Collection<T>> supl){
+
+    public static <T> ValueProxy<Collection<T>> mutableCollection(Supplier<? extends Collection<T>> supl) {
         return new ValueProxy<Collection<T>>() {
             @Override
             public Collection<T> get() {
@@ -23,17 +31,17 @@ public abstract class Extractors {
             @Override
             public void set(Collection<T> v) {
                 Collection<T> get = supl.get();
-                if(v == get){
-                   //same collection, just return
-                   return;
+                if (v == get) {
+                    //same collection, just return
+                    return;
                 }
                 get.clear();
                 get.addAll(v);
-                
+
             }
         };
     }
-    
+
     public static <T> ValueProxy<T> castProxy(Supplier supl, Consumer cons) {
         return new ValueProxy<T>() {
             @Override
@@ -47,8 +55,8 @@ public abstract class Extractors {
             }
         };
     }
-    
-     public static <T> ValueProxy<T> quickProxy(Supplier<? extends T> supl, Consumer<? super T> cons) {
+
+    public static <T> ValueProxy<T> quickProxy(Supplier<? extends T> supl, Consumer<? super T> cons) {
         return new ValueProxy<T>() {
             @Override
             public T get() {
@@ -102,6 +110,133 @@ public abstract class Extractors {
         @Override
         public void set(T v) {
             proxy.set(v);
+        }
+    }
+
+    public static class BasicBeanPropertyAccess<V, T> implements ValueProxy<T> {
+
+        protected final V object;
+        protected final Method read;
+        protected final Method write;
+        protected final String writeMethodName;
+        protected final String readMethodName;
+
+        @Override
+        public T get() {
+            return F.unsafeCall(() -> (T) read.invoke(object));
+        }
+
+        @Override
+        public void set(T v) {
+            F.unsafeRun(() -> {
+                write.invoke(object, v);
+            });
+        }
+
+        public BasicBeanPropertyAccess(V object, String property) {
+            this.object = object;
+            Class clazz = object.getClass();
+            //try simple then boolean
+            writeMethodName = "set" + NameGenerator.capitalize(property);
+
+            write = Stream.of(clazz.getMethods())
+                    .filter(p -> p.getName().equals(writeMethodName))
+                    .filter(p -> p.getParameterCount() == 1)
+                    .findFirst().orElseThrow(() -> new IllegalArgumentException("Failed to find write method of name:" + writeMethodName));
+
+            String simpleReadName = "get" + NameGenerator.capitalize(property);
+
+            Optional<Method> firstTry = Stream.of(clazz.getMethods())
+                    .filter(p -> p.getName().equals(simpleReadName))
+                    .filter(p -> p.getParameterCount() == 0)
+                    .findFirst();
+
+            if (!firstTry.isPresent()) {
+                readMethodName = "is" + NameGenerator.capitalize(property);
+                read = Stream.of(clazz.getMethods())
+                        .filter(p -> p.getName().equals(readMethodName))
+                        .filter(p -> p.getParameterCount() == 0)
+                        .findFirst().orElseThrow(() -> new IllegalArgumentException("Failed to find write method of name:" + simpleReadName + " or " + readMethodName));
+            } else {
+                read = firstTry.get();
+                readMethodName = simpleReadName;
+            }
+
+        }
+
+    }
+
+    private static class NameGenerator {
+
+        private Map<Object, String> valueToName;
+        private Map<String, Integer> nameToCount;
+
+        public NameGenerator() {
+            valueToName = new IdentityHashMap<>();
+            nameToCount = new HashMap<>();
+        }
+
+        /**
+         * Clears the name cache. Should be called to near the end of the
+         * encoding cycle.
+         */
+        public void clear() {
+            valueToName.clear();
+            nameToCount.clear();
+        }
+
+        /**
+         * Returns the root name of the class.
+         */
+        @SuppressWarnings("rawtypes")
+        public static String unqualifiedClassName(Class type) {
+            if (type.isArray()) {
+                return unqualifiedClassName(type.getComponentType()) + "Array";
+            }
+            String name = type.getName();
+            return name.substring(name.lastIndexOf('.') + 1);
+        }
+
+        /**
+         * Returns a String which capitalizes the first letter of the string.
+         */
+        public static String capitalize(String name) {
+            if (name == null || name.length() == 0) {
+                return name;
+            }
+            return name.substring(0, 1).toUpperCase(ENGLISH) + name.substring(1);
+        }
+
+        /**
+         * Returns a unique string which identifies the object instance.
+         * Invocations are cached so that if an object has been previously
+         * passed into this method then the same identifier is returned.
+         *
+         * @param instance object used to generate string
+         * @return a unique string representing the object
+         */
+        public String instanceName(Object instance) {
+            if (instance == null) {
+                return "null";
+            }
+            if (instance instanceof Class) {
+                return unqualifiedClassName((Class) instance);
+            } else {
+                String result = valueToName.get(instance);
+                if (result != null) {
+                    return result;
+                }
+                Class<?> type = instance.getClass();
+                String className = unqualifiedClassName(type);
+
+                Integer size = nameToCount.get(className);
+                int instanceNumber = (size == null) ? 0 : size + 1;
+                nameToCount.put(className, instanceNumber);
+
+                result = className + instanceNumber;
+                valueToName.put(instance, result);
+                return result;
+            }
         }
     }
 }
---
+++ commonslb-tests/src/test/java/regression/datasync/PropertyAccessTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regression.datasync;
+
+import lt.lb.commons.Log;
+import lt.lb.commons.datasync.extractors.Extractors;
+import lt.lb.commons.datasync.extractors.Extractors.BasicBeanPropertyAccess;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ *
+ * @author laim0nas100
+ */
+public class PropertyAccessTest {
+    public class SimpleClass {
+        private boolean ok;
+        private String myName;
+        private String CapName;
+
+        public boolean isOk() {
+            return ok;
+        }
+
+        public void setOk(boolean ok) {
+            this.ok = ok;
+        }
+
+        public String getMyName() {
+            return myName;
+        }
+
+        public void setMyName(String myName) {
+            this.myName = myName;
+        }
+
+        public String getCapName() {
+            return CapName;
+        }
+
+        public void setCapName(String CapName) {
+            this.CapName = CapName;
+        }
+
+        @Override
+        public String toString() {
+            return "SimpleClass{" + "ok=" + ok + ", myName=" + myName + ", CapName=" + CapName + '}';
+        }
+        
+        
+        
+    }
+    
+    @Test
+    public void testProperties(){
+        SimpleClass s = new SimpleClass();
+        s.ok = true;
+        s.myName = "SOME NAME";
+        s.CapName ="CAP NAME";
+        Log.main().async = false;
+        Log.print(s);
+        
+        BasicBeanPropertyAccess<SimpleClass,Boolean> propBoolean = new Extractors.BasicBeanPropertyAccess<>(s,"ok");
+        
+        propBoolean.set(false);
+        assertThat(s.ok).isFalse();
+        BasicBeanPropertyAccess<SimpleClass,String> propName = new Extractors.BasicBeanPropertyAccess<>(s,"myName");
+        propName.set("CHANGED NAME");
+        assertThat(s.myName).isEqualTo("CHANGED NAME");
+        BasicBeanPropertyAccess<SimpleClass,String> capName = new Extractors.BasicBeanPropertyAccess<>(s,"CapName");
+        capName.set("CHANGED CAP NAME");
+        assertThat(s.CapName).isEqualTo("CHANGED CAP NAME");
+        
+        
+    }
+    
+    
+}
