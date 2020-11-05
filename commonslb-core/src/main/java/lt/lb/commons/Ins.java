c4c59000bd505cb36ed2b361f3690ccea46d4801
laim0nas100 <aciukadsiunciate@gmail.com>
2020-11-05 16:43:59 +0200

    primitive promotion/demotion functionality

--- commonslb-core/src/main/java/lt/lb/commons/Ins.java
+++ commonslb-core/src/main/java/lt/lb/commons/Ins.java
@@ -14,15 +14,14 @@ public class Ins<T> {
 
     public static class InsCl<T> extends Ins<T> implements ExtComparable<Class> {
 
-        private InsCl(Class<T> cl, T ob) {
-            super(cl, ob);
+        protected InsCl(boolean primitivePromotion, Class<T> cl, T ob) {
+            super(primitivePromotion, cl, ob);
             if (cl == null) {
                 throw new IllegalArgumentException("Class must be provided");
             }
 
         }
 
-        
         @Override
         public int compareTo(Class o) {
             return Ins.typeComparator.compare(clazz, o);
@@ -166,12 +165,13 @@ public class Ins<T> {
     protected final Class<T> clazz;
     protected final T object;
     protected final boolean isNull;
+    protected final boolean primitivePromotion;
 
-    private Ins(Class<T> cl, T ob) {
+    protected Ins(boolean primitivePromotion, Class<T> cl, T ob) {
         this.clazz = cl;
         this.object = ob;
         this.isNull = object == null && clazz == null;
-
+        this.primitivePromotion = primitivePromotion;
         if (clazz != null && ob != null) {
             throw new IllegalArgumentException("Can only provide one or the other");
         }
@@ -186,7 +186,19 @@ public class Ins<T> {
      */
     public static <T> InsCl<T> of(Class<T> cls) {
         Objects.requireNonNull(cls, "Null should not be a class parameter");
-        return new InsCl(cls, null);
+        return new InsCl(false, cls, null);
+    }
+
+    /**
+     * Create {@code Ins} of given class with primitive promotion
+     *
+     * @param <T>
+     * @param cls
+     * @return
+     */
+    public static <T> InsCl<T> ofPrimitivePromotion(Class<T> cls) {
+        Objects.requireNonNull(cls, "Null should not be a class parameter");
+        return new InsCl(true, cls, null);
     }
 
     /**
@@ -197,7 +209,18 @@ public class Ins<T> {
      * @return
      */
     public static <T> Ins<T> ofNullable(T obj) {
-        return new Ins<>(null, obj);
+        return new Ins<>(false, null, obj);
+    }
+
+    /**
+     * Create {@code Ins} of given object with primitive promotion
+     *
+     * @param <T>
+     * @param obj
+     * @return
+     */
+    public static <T> Ins<T> ofNullablePrimitivePromotion(T obj) {
+        return new Ins<>(true, null, obj);
     }
 
     /**
@@ -288,14 +311,12 @@ public class Ins<T> {
         }
 
     }
-    
-    
 
     protected boolean instanceOf0(Class c) {
         if (clazz == null) {
-            return instanceOf(object, c);
+            return primitivePromotion ? instanceOfPrimitivePromotion(object, c) : instanceOf(object, c);
         } else {
-            return instanceOfClass(clazz, c);
+            return primitivePromotion ? instanceOfClassPrimitivePromotion(clazz, c) : instanceOfClass(clazz, c);
         }
 
     }
@@ -321,6 +342,27 @@ public class Ins<T> {
     }
 
     /**
+     * Null-friendly {@code Class.isAssignableFrom} version with primitive
+     * promotion.
+     *
+     * @param what
+     * @param of
+     *
+     * Static equivalent: what instanceof of
+     * @return
+     */
+    public static boolean instanceOfClassPrimitivePromotion(Class what, Class of) {
+        if (of == null) { // nothing except null is instance of null
+            return what == null;
+        }
+        if (what == null) {
+            return false;
+        } else {
+            return primitivePromotion(of).isAssignableFrom(what) || primitiveDemotion(of).isAssignableFrom(what);
+        }
+    }
+
+    /**
      * Null-friendly {@code Class.isInstance} version.
      *
      * @param what
@@ -338,6 +380,137 @@ public class Ins<T> {
         } else {
             return of.isInstance(what);
         }
+    }
+
+    /**
+     * Null-friendly {@code Class.isInstance} version with primitive promotion.
+     *
+     * @param what
+     * @param of
+     *
+     * Static equivalent: what instanceof of
+     * @return
+     */
+    public static boolean instanceOfPrimitivePromotion(Object what, Class of) {
+        if (of == null) { // nothing except null is instance of null
+            return what == null;
+        }
+        if (what == null) {
+            return false;
+        } else {
+            return primitivePromotion(of).isInstance(what) || primitiveDemotion(of).isInstance(what);
+        }
+    }
+
+    /**
+     * Promote given primitive class to a non-primitive counterpart if possible,
+     * otherwise return the argument. Null-safe.
+     *
+     * @param type
+     * @return
+     */
+    public static Class primitivePromotion(Class type) {
+        if (type == null) {
+            return null;
+        }
+
+        if (!type.isPrimitive()) {
+            return type;
+        }
+
+        if (Boolean.TYPE.equals(type)) {
+            return Boolean.class;
+        }
+        if (Character.TYPE.equals(type)) {
+            return Character.class;
+        }
+
+        if (Integer.TYPE.equals(type)) {
+            return Integer.class;
+        }
+
+        if (Long.TYPE.equals(type)) {
+            return Long.class;
+        }
+
+        if (Float.TYPE.equals(type)) {
+            return Float.class;
+        }
+
+        if (Double.TYPE.equals(type)) {
+            return Double.class;
+        }
+
+        if (Byte.TYPE.equals(type)) {
+            return Byte.class;
+        }
+
+        if (Short.TYPE.equals(type)) {
+            return Short.class;
+        }
+
+        if (Void.TYPE.equals(type)) {
+            return Void.class;
+        }
+
+        return type;
+
+    }
+
+    /**
+     * Demote given non-primitive class to a primitive counterpart if possible,
+     * otherwise return the argument. Null-safe.
+     *
+     * @param type
+     * @return
+     */
+    public static Class primitiveDemotion(Class type) {
+        if (type == null) {
+            return null;
+        }
+
+        if (type.isPrimitive()) {
+            return type;
+        }
+
+        if (Boolean.class.equals(type)) {
+            return Boolean.TYPE;
+        }
+
+        if (Character.class.equals(type)) {
+            return Character.TYPE;
+        }
+
+        if (Integer.class.equals(type)) {
+            return Integer.TYPE;
+        }
+
+        if (Long.class.equals(type)) {
+            return Long.TYPE;
+        }
+
+        if (Float.class.equals(type)) {
+            return Float.TYPE;
+        }
+
+        if (Double.class.equals(type)) {
+            return Double.TYPE;
+        }
+
+        if (Byte.class.equals(type)) {
+            return Byte.TYPE;
+        }
+
+        if (Short.class.equals(type)) {
+            return Short.TYPE;
+        }
+
+        if (Void.class.equals(type)) {
+            return Void.TYPE;
+        }
+
+        return type;
+
     }
 
     protected boolean allNull(Object[] arr, boolean ifNull) {
@@ -379,12 +552,18 @@ public class Ins<T> {
     public static final ExtComparator<Class> typeComparator = new ExtComparator<Class>() {
         @Override
         public int compare(Class o1, Class o2) {
-            if(o1 == null || o2 == null){
-                if(o1 == null && o2 == null){
-                    return 0;
-                }
-                return o1 == null ? -1 : 1;
+            if (o1 == null && o2 == null) {
+                return 0;
             }
+
+            if (o1 == null) {
+                return -1;
+            }
+            
+            if (o2 == null) {
+                return 1;
+            }
+            
             boolean i_1 = instanceOfClass(o1, o2);
             boolean i_2 = instanceOfClass(o2, o1);
             if (i_1 && !i_2) {
@@ -393,12 +572,10 @@ public class Ins<T> {
             if (!i_1 && i_2) {
                 return -1;
             }
-            
-            
+
             return 0;
         }
-        
-        
+
     };
 
 }
