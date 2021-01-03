c74c48dbfddd82111ad921bd1657edc460c0d378
laim0nas100 <aciukadsiunciate@gmail.com>
2021-01-03 06:06:17 +0200

    update

--- commonslb-javafx/src/main/java/lt/lb/commons/javafx/FXDefs.java
+++ commonslb-javafx/src/main/java/lt/lb/commons/javafx/FXDefs.java
@@ -1,18 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lt.lb.commons.javafx;
 
+import java.io.File;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
@@ -20,6 +20,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
@@ -32,10 +35,12 @@ import lt.lb.commons.misc.Range;
 import lt.lb.commons.parsing.NumberParsing;
 import lt.lb.commons.javafx.fxrows.FXDrows;
 import lt.lb.commons.javafx.fxrows.FXDrowsConf;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
- * @author Lemmin
+ * @author laim0nas100
  */
 public abstract class FXDefs {
 
@@ -100,7 +105,7 @@ public abstract class FXDefs {
             });
         }
     }
-    
+
     public static void applyOnTextChangeOrEnter(TextField tf, Consumer<TextField> consumer) {
         StringValue value = new StringValue();
         tf.textProperty().addListener((FXDefs.SimpleChangeListener<String>) s -> {
@@ -111,7 +116,6 @@ public abstract class FXDefs {
             consumer.accept(tf);
         });
     }
-    
 
     public static void applyOnFocusChange(TextField tf, Consumer<TextField> consumer) {
         BooleanValue hasChanges = BooleanValue.FALSE();
@@ -159,6 +163,7 @@ public abstract class FXDefs {
 
     public static class SimpleListViewCell<P> extends ListCell<P> {
 
+        public static Logger logger = LogManager.getLogger(SimpleListViewCell.class);
         Lambda.L3<ListCell<P>, P, Boolean> decorator;
 
         public SimpleListViewCell(Lambda.L3<ListCell<P>, P, Boolean> decorator) {
@@ -173,10 +178,61 @@ public abstract class FXDefs {
                 }
                 super.updateItem(item, empty);
                 decorator.apply(this, item, empty);
-            }).ifPresent(t -> t.printStackTrace());
+            }).ifPresent(t -> logger.error("Error in item update", t));
 
         }
 
+    }
+
+    public static <N extends Node> void configureDoubleClick(N node, Predicate<N> when, Consumer<N> cons) {
+        node.setOnMousePressed((MouseEvent event) -> {
+            if (when.test(node) && event.isPrimaryButtonDown()) {
+                if (event.getClickCount() > 1) {
+                    cons.accept(node);
+                    event.consume();
+                }
+            }
+        });
+    }
+
+    public static void configureDragNDrop(Node root, Predicate<Dragboard> when, Consumer<Dragboard> consumer) {
+        root.setOnDragOver(event -> {
+            Dragboard dragboard = event.getDragboard();
+            if (event.getGestureSource() != root && when.test(dragboard)) {
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
+            event.consume();
+        });
+
+        root.setOnDragDropped(event -> {
+            Dragboard dragboard = event.getDragboard();
+            boolean success = false;
+            if (event.getGestureSource() != root && when.test(dragboard)) {
+                consumer.accept(dragboard);
+                success = true;
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
+    }
+
+    public static void configureDragNDropFiles(Node root, Predicate<File> when, Consumer<List<File>> consumer) {
+        configureDragNDrop(root, d -> {
+            if (!d.hasFiles()) {
+                return false;
+            }
+            List<File> files = d.getFiles();
+            for (File f : files) {
+                if (when.test(f)) {
+                    return true;
+                }
+            }
+            return false;
+        }, d -> {
+            List<File> files = d.getFiles();
+            List<File> collect = files.stream().filter(when).collect(Collectors.toList());
+            consumer.accept(collect);
+        });
     }
 
     public static <P> SimpleListViewCallback<P> cellFactory(Lambda.L3<ListCell<P>, P, Boolean> decorator) {
