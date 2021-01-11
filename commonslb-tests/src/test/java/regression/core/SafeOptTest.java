55e7ef459509f467614b3d9f6c3fe4f885dd9854
laim0nas100 <aciukadsiunciate@gmail.com>
2021-01-12 01:54:51 +0200

    SafeOpt test

--- commonslb-tests/src/test/java/regression/core/SafeOptTest.java
+++ commonslb-tests/src/test/java/regression/core/SafeOptTest.java
@@ -19,19 +19,34 @@ public class SafeOptTest {
     @Test
     public void test() {
         SafeOpt<Number> num = SafeOpt.of(10L).select(Long.class);
-        
+
         assertThat(num.isPresent());
-        
+
         SafeOpt<Integer> map = SafeOpt.of(10).map(m -> m * 10);
         Integer expected = 10 * 10;
         assertThat(map.get()).isEqualTo(expected);
 
-        SafeOpt<Integer> mapEx = map.map(m -> m + new IntegerValue().get());
+        SafeOpt<String> empty = SafeOpt.empty().map(m -> m + "");
+
+        assertThat(empty.isEmpty());
+        assertThat(empty.getError().isEmpty());
+        SafeOpt<Object> errored = SafeOpt.empty(new RuntimeException("Failed"));
+        assertThat(errored.isEmpty());
+        assertThat(errored.getError().isPresent());
+
+        SafeOpt<Integer> mapEx = map.map(m -> m + new IntegerValue().get()); // supposed to be null
         ThrowableTypeAssert<NoSuchElementException> noSuchElement = Assertions.assertThatExceptionOfType(NoSuchElementException.class);
         noSuchElement.isThrownBy(() -> mapEx.get());
         ThrowableTypeAssert<NestedException> nestedException = Assertions.assertThatExceptionOfType(NestedException.class);
         nestedException.isThrownBy(() -> mapEx.asOptional().get());
 
+        Assertions.assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> {
+            try {
+                mapEx.asOptional().get();
+            } catch (NestedException ex) {
+                throw ex.getCause();
+            }
+        });
         ThrowableTypeAssert<NestedException> nested = Assertions.assertThatExceptionOfType(NestedException.class);
 
         nested.isThrownBy(() -> mapEx.throwIfErrorNested());
