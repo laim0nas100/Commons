e72ea08d1d5e829497354fb2bcdcde53d61e92a5
laim0nas100 <aciukadsiunciate@gmail.com>
2021-10-03 21:55:05 +0300

    introduce task executor queue with basic implementation

---
+++ commonslb-threads/src/main/java/lt/lb/commons/threads/service/BasicTaskExecutorQueue.java
@@ -0,0 +1,208 @@
+package lt.lb.commons.threads.service;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.Future;
+import lt.lb.commons.containers.values.IntegerValue;
+import lt.lb.commons.threads.Futures;
+import lt.lb.commons.threads.service.BasicTaskExecutorQueue.BasicRunInfo;
+import lt.lb.commons.threads.service.TaskExecutorQueue.RunInfo;
+import lt.lb.commons.threads.sync.AtomicMap;
+import lt.lb.uncheckedutils.func.UncheckedRunnable;
+
+/**
+ *
+ * @author laim0nas100
+ */
+public abstract class BasicTaskExecutorQueue implements TaskExecutorQueue<String, BasicRunInfo> {
+
+    public static class BasicRunStateResult {
+
+        public final BasicRunState state;
+        public final boolean result;
+
+        public BasicRunStateResult(BasicRunState state, boolean result) {
+            this.state = Objects.requireNonNull(state);
+            this.result = result;
+        }
+
+        public BasicRunStateResult allow() {
+            if (result == true) {
+                return this;
+            }
+            return new BasicRunStateResult(state, true);
+        }
+
+        public BasicRunStateResult deny() {
+            if (result == false) {
+                return this;
+            }
+            return new BasicRunStateResult(state, false);
+        }
+
+    }
+
+    public static class BasicRunState {
+
+        public final Thread runner;
+
+        public int nested;
+
+        public BasicRunState(Thread runner) {
+            this.runner = runner;
+        }
+
+        public BasicRunState() {
+            this(Thread.currentThread());
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 11 * hash + Objects.hashCode(this.runner);
+            hash = 11 * hash + this.nested;
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final BasicRunState other = (BasicRunState) obj;
+            if (this.nested != other.nested) {
+                return false;
+            }
+            return Objects.equals(this.runner, other.runner);
+        }
+
+    }
+
+    public static class BasicRunInfo implements RunInfo<String> {
+
+        public final boolean inPlace;
+        public final boolean reentrant;
+        public final boolean unique;
+        public final String key;
+        public final String name;
+
+        public BasicRunInfo(boolean reenterant, boolean unique, boolean inPlace, String key, String name) {
+            this.reentrant = reenterant;
+            this.unique = unique;
+            this.inPlace = inPlace;
+            this.key = Objects.requireNonNull(key);
+            this.name = name;
+        }
+
+        public static BasicRunInfo unique(String key, String name) {
+            return new BasicRunInfo(true, true, false, key, name);
+        }
+
+        public static BasicRunInfo uniqueInPlace(String key, String name) {
+            return new BasicRunInfo(true, true, true, key, name);
+        }
+
+        public static BasicRunInfo basic(boolean inPlace, String name) {
+            return new BasicRunInfo(true, false, inPlace, String.valueOf(name), name);
+        }
+
+        @Override
+        public boolean isUnique() {
+            return unique;
+        }
+
+        @Override
+        public String getKey() {
+            return key;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+    }
+
+    protected AtomicMap<String, BasicRunStateResult> states = new AtomicMap<>();
+
+    protected ThreadLocal<IntegerValue> inside = ThreadLocal.withInitial(() -> new IntegerValue(0));
+
+    @Override
+    public boolean tryEnqueue(BasicRunInfo info) {
+        if (!info.isUnique()) { // non unique can always go
+            return true;
+        }
+
+        BasicRunState stateNew = new BasicRunState();
+
+        return states.changeSupplyIfAbsent(info.getKey(), () -> new BasicRunStateResult(stateNew, true), s -> {
+            if (s == null) {
+                return new BasicRunStateResult(stateNew, true);
+            }
+            if (s.state == stateNew) {
+                return s.allow();
+            }
+            if (info.reentrant) {
+                if (Objects.equals(s.state.runner, stateNew.runner)) {
+                    s.state.nested++;// same thread
+                    return s.allow();
+                }
+            }
+            return s.deny();
+
+        }).result;
+
+    }
+
+    @Override
+    public void dequeue(boolean ran, BasicRunInfo info) {
+        if (!ran) {
+            return;// no information was left
+        }
+        if (info.reentrant || info.inPlace) {
+            inside.get().decrementAndGet();
+        }
+        if (info.isUnique()) {
+            states.changeIfPresent(info.getKey(), state -> {
+                if (info.reentrant) {
+                    if (state.state.nested <= 1) {
+                        return null;
+                    } else {
+                        state.state.nested--;
+                        return state;
+                    }
+                }
+                return null;
+            });
+        }
+    }
+
+    @Override
+    public Future<Optional<Throwable>> enqueue(BasicRunInfo info, UncheckedRunnable run) {
+        //assume we are enqued
+
+        if (info.inPlace || (info.reentrant && inside.get().get() > 1)) {
+            inside.get().incrementAndGet();
+            Optional<Throwable> error = runUnbounded(info, run);
+            dequeue(true, info);
+            return Futures.done(error);
+        }
+
+        return getExecutor().submit(() -> {
+            if (info.reentrant) {
+                inside.get().incrementAndGet();
+            }
+            Optional<Throwable> error = runUnbounded(info, run);
+            dequeue(true, info);
+            return error;
+        });
+
+    }
+
+}
---
+++ commonslb-threads/src/main/java/lt/lb/commons/threads/service/TaskExecutorQueue.java
@@ -0,0 +1,95 @@
+package lt.lb.commons.threads.service;
+
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import lt.lb.commons.threads.Futures;
+import lt.lb.commons.threads.service.TaskExecutorQueue.RunInfo;
+import lt.lb.uncheckedutils.Checked;
+import lt.lb.uncheckedutils.PassableException;
+import lt.lb.uncheckedutils.SafeOpt;
+import lt.lb.uncheckedutils.func.UncheckedRunnable;
+
+/**
+ *
+ * @author laim0nas100
+ */
+public interface TaskExecutorQueue<P extends Serializable, R extends RunInfo<P>> {
+
+    public static interface RunInfo<P extends Serializable> {
+
+        public boolean isUnique();
+
+        public P getKey();
+
+        public default String getName() {
+            return String.valueOf(getKey());
+        }
+    }
+
+    public ScheduledExecutorService getScheduler();
+
+    public ExecutorService getExecutor();
+
+    public default ScheduledFuture<?> schedulePeriodically(R info, long first, long period, TimeUnit unit, UncheckedRunnable run) {
+        return getScheduler().scheduleAtFixedRate(() -> {
+            submit(info, run);
+        }, first, period, unit);
+    }
+
+    public default ScheduledFuture<Future<Optional<Throwable>>> schedule(R info, boolean unique, long first, TimeUnit unit, UncheckedRunnable run) {
+        return getScheduler().schedule(() -> {
+            return submit(info, run);
+        }, first, unit);
+    }
+
+    public default Future<Optional<Throwable>> submit(R info, UncheckedRunnable run) {
+        if (tryEnqueue(info)) {
+            return enqueue(info, run);
+        } else {
+            dequeue(false, info);
+        }
+        return Futures.done(onFailedEnqueue(info));
+    }
+
+    public default Optional<Throwable> submitMap(R info, UncheckedRunnable run) {
+        return SafeOpt.ofFuture(submit(info, run)).flatMapOpt(m -> m).asOptional();
+    }
+
+    /**
+     * Should call dequeue itself after completing the task
+     *
+     * @param info
+     * @param run
+     * @return
+     */
+    public Future<Optional<Throwable>> enqueue(R info, UncheckedRunnable run);
+
+    public boolean tryEnqueue(R info);
+
+    public void dequeue(boolean ran, R info);
+
+    public default Optional<Throwable> onFailedEnqueue(R info) {
+        return Optional.of(new PassableException("Failed to enqueue " + info.getName() + " with key:" + info.getKey()));
+    }
+
+    public default Optional<Throwable> runUnbounded(R info, UncheckedRunnable run) {
+
+        Optional<Throwable> checkedRun = Checked.checkedRun(beforeRun(info, run));
+        afterRun(info, checkedRun);
+        return checkedRun;
+    }
+
+    public default UncheckedRunnable beforeRun(R info, UncheckedRunnable run) {
+        return run;
+    }
+
+    public default void afterRun(R info, Optional<Throwable> error) {
+
+    }
+
+}
