eed8b21d44307149c1938443356a169bd4a2dfc8
laim0nas100 <aciukadsiunciate@gmail.com>
2022-12-19 02:09:17 +0200

    Add request commiter test using javaFX

---
+++ commonslb-tests/src/test/java/empiric/threading/RequestServiceCommiterTest.java
@@ -0,0 +1,48 @@
+package empiric.threading;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import lt.lb.commons.DLog;
+import lt.lb.commons.javafx.FXDefs;
+import lt.lb.commons.javafx.fxrows.FXDrows;
+import lt.lb.commons.javafx.scenemanagement.MultiStageManager;
+import lt.lb.commons.javafx.scenemanagement.StageFrame;
+import lt.lb.commons.threads.executors.FastExecutor;
+import lt.lb.commons.threads.executors.FastWaitingExecutor;
+import lt.lb.commons.threads.service.ServiceRequestCommiter;
+import lt.lb.commons.threads.sync.WaitTime;
+
+/**
+ *
+ * @author Lemmin
+ */
+public class RequestServiceCommiterTest {
+    
+    
+    public static void main(String[] args) throws Exception{
+        
+        //ScheduledExecutorService service, Executor exe, WaitTime time, WaitTime timeout, long untimedRequestThreashold, Callable<T> call, long timedRequestThreshold
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(1);
+        FastExecutor executor = new FastWaitingExecutor(4, WaitTime.ofSeconds(5));
+        ServiceRequestCommiter serviceRequestCommiter = new ServiceRequestCommiter(pool,executor, WaitTime.ofSeconds(2),WaitTime.ofSeconds(50), 20, ()-> {
+            DLog.print("MAIN");
+            return null;
+        }, 10);
+        
+        
+        MultiStageManager manager = new MultiStageManager(RequestServiceCommiterTest.class.getClassLoader());
+        
+        
+        FXDrows rows = FXDefs.fxrows();
+        
+        rows.getNew()
+                .addButton("PUSH ME", eh->{
+                    serviceRequestCommiter.addRequest();
+                })
+                .display();
+        Future<StageFrame> newFormFrame = manager.newFxrowsFrame("NEW WINDOW", rows);
+        
+        newFormFrame.get().show();
+    }
+}
