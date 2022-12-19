a1355bafdc5cb14505a6e51c19d0c05ba6072dab
laim0nas100 <aciukadsiunciate@gmail.com>
2022-12-19 02:05:13 +0200

    Nest IterProvider

--- commonslb-iteration/src/main/java/lt/lb/commons/iteration/NestingIteration.java
+++ commonslb-iteration/src/main/java/lt/lb/commons/iteration/NestingIteration.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
-import lt.lb.commons.ArrayOp;
 import lt.lb.commons.containers.CastIndexedList;
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -14,6 +13,13 @@ import org.apache.commons.lang3.ArrayUtils;
  * @author laim0nas100
  */
 public class NestingIteration<T> {
+
+    public static interface IterProvider<T> {
+
+        public int size();
+
+        public T get(int i);
+    }
 
     public static <T> Iterator<CastIndexedList<T>> iterator(List<IterProvider<T>> list, boolean revPrint) {
         final int size = list.size();
@@ -84,7 +90,7 @@ public class NestingIteration<T> {
         };
     }
 
-    public static class NestedListInfo<T> {
+    private static class NestedListInfo<T> {
 
         int listSize;
         boolean listScanComplete;
@@ -92,107 +98,124 @@ public class NestingIteration<T> {
         Iterator<T> iterator;
     }
 
-    public static <T> Iterator<CastIndexedList<T>> lazyInitIterator(List<Iterator<T>> list, boolean revPrint) {
-        final int size = list.size();
-        boolean isEmpty = true;
+    public static class LazyInitIterator<T> implements Iterator<CastIndexedList<T>> {
 
-        final NestedListInfo<T>[] info = new NestedListInfo[size];
+        protected final int sizeOfIterators;
+        protected final NestedListInfo<T>[] info;
 
-        final int[] listIndex = new int[size];
+        protected final int[] listIndex;
 
-        for (int i = 0; i < size; i++) {
+        protected boolean reachedEnd = false;
 
-            NestedListInfo inf = new NestedListInfo();
-            info[i] = inf;
-            Iterator<T> iterator = list.get(i);
-            inf.iterator = iterator;
-            if (iterator.hasNext()) {
-                isEmpty = false;
-                inf.listSize = 1;
-                inf.completedList = new ArrayList();
-            } else {
-                inf.listScanComplete = true;
-                inf.listSize = 0;
-                inf.completedList = EmptyImmutableList.getInstance();
-            }
+        public boolean revPrint;
 
-            listIndex[i] = 0;
+        public LazyInitIterator(List<Iterator<T>> list, boolean revPrint) {
+            this(list);
+            this.revPrint = revPrint;
         }
 
-        if (isEmpty) {
-            return EmptyImmutableList.emptyIterator();
-        }
+        public LazyInitIterator(List<Iterator<T>> list) {
+            this.sizeOfIterators = list.size();
+            info = new NestedListInfo[sizeOfIterators];
+            listIndex = new int[sizeOfIterators];
 
-        return new Iterator<CastIndexedList<T>>() {
-            boolean reachedEnd = false;
+            for (int i = 0; i < sizeOfIterators; i++) {
 
-            @Override
-            public boolean hasNext() {
-                return !reachedEnd;
-            }
-
-            @Override
-            public CastIndexedList<T> next() {
-                if (hasNext()) {
-                    List<T> args = new ArrayList<>(size);
-                    for (int i = 0; i < size; i++) {
-                        //if can get from completed, do it, otherwise get from iterators
-                        int localIndex = listIndex[i];
-                        T item;
-                        NestedListInfo<T> inf = info[i];
-                        if (inf.completedList.size() > localIndex) {
-                            item = inf.completedList.get(localIndex);
-                        } else if (inf.iterator.hasNext()) {
-                            // this is ensured to have next
-                            item = inf.iterator.next();
-                            inf.completedList.add(item);
-                        } else {
-                            item = null;
-                        }
-                        args.add(item);
-                    }
-                    int[] indexes = ArrayUtils.clone(listIndex);
-                    if (revPrint) {
-                        ArrayUtils.reverse(indexes);
-                        Collections.reverse(args);
-                    }
-                    onConsume();
-                    return new CastIndexedList<>(indexes, args);
+                NestedListInfo inf = new NestedListInfo();
+                info[i] = inf;
+                Iterator<T> iterator = list.get(i);
+                inf.iterator = iterator;
+                if (iterator.hasNext()) {
+                    inf.listSize = 1;
+                    inf.completedList = new ArrayList();
                 } else {
-                    throw new NoSuchElementException();
+                    inf.listScanComplete = true;
+                    inf.listSize = 0;
+                    inf.completedList = EmptyImmutableList.getInstance();
                 }
+
+                listIndex[i] = 0;
             }
+        }
 
-            private void onConsume() {
-                boolean end = true;
-                for (int i = 0; i < size; i++) {
+        @Override
+        public boolean hasNext() {
+            return !reachedEnd;
+        }
+
+        @Override
+        public CastIndexedList<T> next() {
+            if (hasNext()) {
+                List<T> args = new ArrayList<>(sizeOfIterators);
+                for (int i = 0; i < sizeOfIterators; i++) {
+                    //if can get from completed, do it, otherwise get from iterator
+                    int localIndex = listIndex[i];
+                    T item;
                     NestedListInfo<T> inf = info[i];
-                    if (inf.listScanComplete) {
-                        continue;
+                    if (inf.completedList.size() > localIndex) {
+                        item = inf.completedList.get(localIndex);
+                    } else if (inf.iterator.hasNext()) {
+                        // this is ensured to have next
+                        item = inf.iterator.next();
+                        inf.completedList.add(item);
+                    } else {
+                        item = null;
                     }
-                    List<T> completed = inf.completedList;
-                    int completedSize = completed.size();
-                    boolean hasNext = inf.iterator.hasNext();
+                    args.add(item);
+                }
+                int[] indexes = ArrayUtils.clone(listIndex);//save copy because on consume modifies
+                onConsume();
 
-                    if (!hasNext) {
-                        inf.listScanComplete = true;
-                    } else if (listIndex[i] + 1 >= completedSize && inf.listSize == completedSize) {
+                return new CastIndexedList<>(indexes, args);
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        protected CastIndexedList<T> produce(int[] indexes, List<T> args) {
+            if (revPrint) {
+                ArrayUtils.reverse(indexes);
+                Collections.reverse(args);
+            }
+            return new CastIndexedList<>(indexes, args);
+        }
+
+        protected void onConsume() {
+            boolean end = true;
+            for (int i = 0; i < sizeOfIterators; i++) {
+                NestedListInfo<T> inf = info[i];
+                if (inf.listScanComplete) {
+                    continue;
+                }
+
+                boolean hasNext = inf.iterator.hasNext();
+
+                if (!hasNext) {
+                    inf.listScanComplete = true;
+                } else {
+                    int completedSize = inf.completedList.size();
+                    if (listIndex[i] + 1 >= completedSize && inf.listSize == completedSize) {
                         inf.listSize++;
                     }
                 }
-
-                for (int i = 0; i < size; i++) {
-                    if (listIndex[i] + 1 < info[i].listSize) {
-                        listIndex[i]++;
-                        for (int j = 0; j < i; j++) {
-                            listIndex[j] = 0;
-                        }
-                        end = false;
-                        break;
-                    }
-                }
-                reachedEnd = end;
             }
-        };
+
+            for (int i = 0; i < sizeOfIterators; i++) {
+                if (listIndex[i] + 1 < info[i].listSize) {// found list index to increment
+                    listIndex[i]++;
+                    for (int j = 0; j < i; j++) {
+                        listIndex[j] = 0;
+                    }
+                    end = false;
+                    break;
+                }
+            }
+            reachedEnd = end;
+        }
+
+    }
+
+    public static <T> Iterator<CastIndexedList<T>> lazyInitIterator(List<Iterator<T>> list, boolean revPrint) {
+        return new LazyInitIterator<>(list);
     }
 }
